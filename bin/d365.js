fa39c8775ff6fc53c1fe528fb1ce8563c9481041
Derek Finlinson <dcfinlinson@gmail.com>
2018-06-21 15:23:11 -0600

    Basic project structure and skeletons

---
+++ bin/d365.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const program = require("commander"),
+    boxen = require("boxen"),
+    chalk = require("chalk");
+
+console.log(boxen(chalk.blue.bold("Dynamics 365 CLI"), { padding: 1, borderStyle: "classic"}));
+
+// Add empty line
+console.log();
+
+program
+    .version(require('../package').version)
+    .usage("<command> [options]");
+
+// Create command
+program
+    .command("create <project> [name]")
+    .description("Create a new project")
+    .action((project, name) => {
+        require("../lib/create")(project, name);
+    });
+
+// Add command
+program
+    .command("add <type> <name>")
+    .description("Add a new file")
+    .action((type, name) => {
+        require("../lib/add")(type, name);
+    });
+
+// Show help on unknown command
+program
+  .arguments('<command>')
+  .action((cmd) => {
+    program.outputHelp();
+    console.log();
+    console.log(chalk.red(`Unknown command ${chalk.yellow(cmd)}.`));
+    console.log();
+  });
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
+
