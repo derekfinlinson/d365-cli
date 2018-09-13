cb01156c8b9f9a62dae2e35aa5682dbb4367df1e
Derek Finlinson <Derek.Finlinson@journeyteam.com>
2018-09-13 11:58:30 -0600

    Start on plugin/workflow templates

---
+++ lib/templates/add/plugin.cs
@@ -0,0 +1,18 @@
+using JourneyTeam.Xrm.Plugin;
+using System;
+
+namespace <%= namespace %>
+{
+    public class <%= name %> : BasePlugin
+    {
+        public override void RegisterEvents()
+        {
+          // Add Registered Events
+        }
+
+        public override void ExecutePlugin(IExtendedPluginContext context)
+        {
+            // Plugin logic
+        }
+    }
+}
